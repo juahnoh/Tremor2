eeea10d7ee5888149ae17c5507fef2b42a127460
juahnoh <139471569+juahnoh@users.noreply.github.com>
2023-09-11 21:03:24 +0900

    Update Pose1Record_screen.dart

--- lib/Pose1/Pose1Record_screen.dart
+++ lib/Pose1/Pose1Record_screen.dart
@@ -41,6 +41,8 @@ class _Pose1RecordState extends State<Pose1Record> {
       if (_secondsLeft == 0) {
         _isRecording=false;
         timer.cancel();
+        _stopAccelerometer();
+        _stopGyroscope();
       }
       if (_isRecording) {
         setState(() {
